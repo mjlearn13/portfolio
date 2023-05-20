b8d00b2a961a40560a50dbd07ee0706831f0d3c4
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-05-20 12:49:56 +0100

    add JS projects

--- src/allProjectsData.jsx
+++ src/allProjectsData.jsx
@@ -8,52 +8,319 @@ import speedTypingGameSquare from '../src/assets/speedTypingGame-square.png'
 import speedTypingGame from '../src/assets/speedTypingGame.png'
 import tenziesSquare from '../src/assets/tenzies-square.png'
 import tenzies from '../src/assets/tenzies.png'
+import memeGenerator from '../src/assets/meme-generator.png'
+import twimbaSquare from '../src/assets/twimba-square.png'
+import twimba from '../src/assets/twimba.png'
+import splashSquare from '../src/assets/splash-square.png'
+import splash from '../src/assets/splash.png'
+import paint from '../src/assets/paint.png'
+import dragSquare from '../src/assets/drag-square.png'
+import drag from '../src/assets/drag.png'
+import calculator from '../src/assets/calculator.png'
+import basketball from '../src/assets/basketball.png' 
+import nasaSquare from '../src/assets/nasa-square.png'
+import nasa from '../src/assets/nasa.png'
+import spockSquare from '../src/assets/spock-square.png'
+import spock from '../src/assets/spock.png'
+import pong from '../src/assets/pong.png'
+import video from '../src/assets/video.png'
+import navigation from '../src/assets/navigation.png'
+import template from '../src/assets/template.png'
+import infinite from '../src/assets/infinite.png'
+import joke from '../src/assets/joke.png'
+import bookmark from '../src/assets/bookmark.png'
+import countdown from '../src/assets/countdown.png'
+import form from '../src/assets/form.png'
+import math from '../src/assets/math-sprint.png'
+import mode from '../src/assets/mode.png'
+import music from '../src/assets/music-player.png'
+import picture from '../src/assets/picture.png'
+import quote from '../src/assets/quote.png'
+import dashboard from '../src/assets/dashboard.png'
+import lead from '../src/assets/lead.png'
+import password from '../src/assets/password.png'
 
-export default  [
+
+
+export default [
   {
     id: '1',
-    name: 'van-life',
+    name: 'VANLIFE',
     url: 'https://inquisitive-zuccutto-886fd5.netlify.app/',
     imageSquare: vanLifeSquare,
     image: vanLife,
-    type: 'react',
+    type: 'React',
     github: 'https://github.com/mjlearn13/van-life.git',
   },
   {
     id: '2',
-    name: 'notes-app',
+    name: 'Notes',
     url: 'https://voluble-brioche-cf4ae0.netlify.app/',
     imageSquare: notesAppSquare,
     image: notesApp,
-    type: 'react',
+    type: 'React',
     github: 'https://github.com/mjlearn13/notes-app.git',
   },
   {
     id: '3',
-    name: 'pic-some',
+    name: 'Pic Some',
     url: 'https://deluxe-churros-f1e05b.netlify.app/',
     imageSquare: picSomeSquare,
     image: picSome,
-    type: 'react',
+    type: 'React',
     github: 'https://github.com/mjlearn13/pic-some.git',
   },
   {
     id: '4',
-    name: 'speedTypingGame',
+    name: 'Speed Typing',
     url: 'https://idyllic-cascaron-fc1a88.netlify.app/',
     imageSquare: speedTypingGameSquare,
     image: speedTypingGame,
-    type: 'react',
+    type: 'React',
     github: 'https://github.com/mjlearn13/speedTypingGame.git',
   },
   {
     id: '5',
-    name: 'tenzies',
+    name: 'Tenzies',
     url: 'https://fastidious-chaja-ae6f30.netlify.app/',
     imageSquare: tenziesSquare,
     image: tenzies,
-    type: 'react',
+    type: 'React',
     github: 'https://github.com/mjlearn13/tenzies.git',
+  },
+  {
+    id: '6',
+    name: 'Meme Generator',
+    url: 'https://benevolent-begonia-d8c730.netlify.app/',
+    imageSquare: memeGenerator,
+    image: memeGenerator,
+    type: 'React',
+    github: 'https://github.com/mjlearn13/meme-generator.git',
+  },
+  {
+    id: '7',
+    name: 'Twimba',
+    url: 'https://mjlearn13.github.io/twitter-clone/',
+    imageSquare: twimbaSquare,
+    image: twimba,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/twitter-clone.git',
+  },
+  {
+    id: '8',
+    name: 'Splash',
+    url: 'https://mjlearn13.github.io/splash-page/',
+    imageSquare: splashSquare,
+    image: splash,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/splash-page.git',
+  },
+  {
+    id: '9',
+    name: 'Paint Clone',
+    url: 'https://mjlearn13.github.io/paint-clone/',
+    imageSquare: paint,
+    image: paint,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/paint-clone.git',
+  },
+  {
+    id: '10',
+    name: 'Kanban',
+    url: 'https://mjlearn13.github.io/drag-and-drop/',
+    imageSquare: dragSquare,
+    image: drag,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/drag-and-drop.git',
+  },
+  {
+    id: '11',
+    name: 'Calculator',
+    url: 'https://mjlearn13.github.io/calculator/',
+    imageSquare: calculator,
+    image: calculator,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/calculator.git',
+  },
+  {
+    id: '12',
+    name: 'Math Sprint Game',
+    url: 'https://mjlearn13.github.io/math-sprint-game/',
+    imageSquare: math,
+    image: math,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/math-sprint-game.git',
+  },
+  {
+    id: '13',
+    name: 'Nasa Api Pictures',
+    url: 'https://mjlearn13.github.io/nasa-api-pictures/',
+    imageSquare: nasaSquare,
+    image: nasa,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/nasa-api-pictures.git',
+  },
+  {
+    id: '14',
+    name: 'Spock Rock',
+    url: 'https://mjlearn13.github.io/spock-rock-game/',
+    imageSquare: spockSquare,
+    image: spock,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/spock-rock-game.git',
+  },
+  {
+    id: '15',
+    name: 'Pong',
+    url: 'https://mjlearn13.github.io/pong/',
+    imageSquare: pong,
+    image: pong,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/pong.git',
+  },
+  {
+    id: '16',
+    name: 'Video Player',
+    url: 'https://mjlearn13.github.io/video-player/',
+    imageSquare: video,
+    image: video,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/video-player.git',
+  },
+  {
+    id: '17',
+    name: 'Animated Navigation',
+    url: 'https://mjlearn13.github.io/animated-navigation/',
+    imageSquare: navigation,
+    image: navigation,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/animated-navigation.git',
+  },
+  {
+    id: '18',
+    name: 'Animated Template',
+    url: 'https://mjlearn13.github.io/animated-template/',
+    imageSquare: template,
+    image: template,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/animated-template.git',
+  },
+  {
+    id: '19',
+    name: 'Infinite Scroll',
+    url: 'https://mjlearn13.github.io/infinite-scroll/',
+    imageSquare: infinite,
+    image: infinite,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/infinite-scroll.git',
+  },
+  {
+    id: '20',
+    name: 'Joke Teller',
+    url: 'https://mjlearn13.github.io/joke-teller/',
+    imageSquare: joke,
+    image: joke,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/joke-teller.git',
+  },
+  {
+    id: '21',
+    name: 'Bookmark',
+    url: 'https://mjlearn13.github.io/bookmark-app/',
+    imageSquare: bookmark,
+    image: bookmark,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/bookmark-app.git',
+  },
+  {
+    id: '22',
+    name: 'Custom Countdown',
+    url: 'https://mjlearn13.github.io/custom-countdown/',
+    imageSquare: countdown,
+    image: countdown,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/custom-countdown.git',
+  },
+  {
+    id: '23',
+    name: 'Form Validation',
+    url: 'https://mjlearn13.github.io/form-validation/',
+    imageSquare: form,
+    image: form,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/form-validation.git',
+  },
+  {
+    id: '24',
+    name: 'Basketball Scorecard',
+    url: 'https://mjlearn13.github.io/basketball-scorecard/',
+    imageSquare: basketball,
+    image: basketball,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/basketball-scorecard.git',
+  },
+  {
+    id: '25',
+    name: 'Light/Dark Mode',
+    url: 'https://mjlearn13.github.io/light-dark-mode/',
+    imageSquare: mode,
+    image: mode,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/light-dark-mode.git',
+  },
+  {
+    id: '26',
+    name: 'Music Player',
+    url: 'https://mjlearn13.github.io/music-player/',
+    imageSquare: music,
+    image: music,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/music-player.git',
+  },
+  {
+    id: '27',
+    name: 'Picture In Picture',
+    url: 'https://mjlearn13.github.io/picture-in-picture/',
+    imageSquare: picture,
+    image: picture,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/picture-in-picture.git',
+  },
+  {
+    id: '28',
+    name: 'Quote Generator',
+    url: 'https://mjlearn13.github.io/quote-generator/',
+    imageSquare: quote,
+    image: quote,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/quote-generator.git',
+  },
+  {
+    id: '29',
+    name: 'Dashboard',
+    url: 'https://mjlearn13.github.io/dashboard/',
+    imageSquare: dashboard,
+    image: dashboard,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/dashboard.git',
+  },
+  {
+    id: '30',
+    name: 'Lead Tracker',
+    url: 'https://mjlearn13.github.io/lead-tracker/',
+    imageSquare: lead,
+    image: lead,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/lead-tracker.git',
+  },
+  {
+    id: '31',
+    name: 'Password Generator',
+    url: 'https://mjlearn13.github.io/password-generator/',
+    imageSquare: password,
+    image: password,
+    type: 'JavaScript',
+    github: 'https://github.com/mjlearn13/password-generator.git',
   },
 ]
 
