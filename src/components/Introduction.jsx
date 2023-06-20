c799e70150467dc3d0b47f924a8064dc8f49efe2
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-06-20 17:04:55 +0100

    update intro text

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -37,25 +37,25 @@ export default function Introduction() {
       <section className="about-me">
         <h2 className="section__title section__title--about">Who I am</h2>
         <p className="section__subtitle section__subtitle--about">
-          Front-End Developer and martial artist from Berkshire.
+          Front-End Developer and martial artist from Berkshire, U.K.
         </p>
 
         <div>
           <p>
-            I'm a web developer based out of Berkshire, UK. My specialities
+            I&apos;m a Front-End React Developer based out of Berkshire, UK. My specialities
             include React JS, JavaScript, CSS and HTML. My focus is on
             developing user-friendly web applications with a strong emphasis on
             responsiveness.
           </p>
           <p>
-            I'm a passionate and self-motivated developer who is eager to learn
-            and develop.
+            I&apos;m passionate, self-motivated and eager to
+            learn and develop.
           </p>
           <p>
-            I was a London Fire-Fighter for 14 years and ran a plumbing/heating/gas 
-            business at the same time. I have an Associate in Specialised Technology 
-            degree in Personal Computer and Network Technology from Allentown Business 
-            School. I'm a purple belt in Brazilian Jiu-Jitsu and a wrestler.
+            I was a London Fire-Fighter for 14 years and also a plumber, carrying out heating and gas work. 
+            I have an Associate in Specialised Technology degree in Personal Computer and Network
+            Technology from Allentown Business School. I&apos;m a purple belt in
+            Brazilian Jiu-Jitsu and a wrestler.
           </p>
         </div>
 
