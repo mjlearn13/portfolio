9fe317ed11c34e3ab21149ea6dbc9a1808e67e69
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-06-21 11:43:00 +0100

    modify footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,31 @@
 export default function Footer(){
     return (
-           <footer className="footer">
-                <a href="mailto:mjlearn13@gmail.com" className="footer__link">mjlearn13@gmail.com</a>
-                <ul className="social-list">
-                    <li className="social-list__item">
-                        <a className="social-list__link" href="https://linkedin.com">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                    </li>
-                    <li className="social-list__item">
-                        <a className="social-list__link" href="https://github.com">
-                            <i className="fab fa-github"></i>
-                        </a>
-                    </li>
-                </ul>
-            </footer>
+      <footer className="footer">
+        <a href="mailto:mjlearn13@gmail.com" className="footer__link">
+          mjlearn13@gmail.com
+        </a>
+        <ul className="social-list">
+          <li className="social-list__item">
+            <a
+              className="social-list__link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://linkedin.com"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li className="social-list__item">
+            <a
+              className="social-list__link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/mjlearn13"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+        </ul>
+      </footer>
     )
 }
