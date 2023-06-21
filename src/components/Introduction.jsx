4a4ff41d5da067a59162b36294f7037da8a979e3
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-06-21 14:28:29 +0100

    add tech-stack to intro

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from 'react'
 import mjHeadShot from '../assets/mj-headShot.jpg'
 import mjBjj from '../assets/mj-bjj.jpeg'
+import reactLogo from '../assets/logos/reactLogo.png'
+import javaScriptLogo from '../assets/logos/javaScriptLogo.png'
+import cssLogo from '../assets/logos/cssLogo.png'
+import firebaseLogo from '../assets/logos/firebaseLogo.png'
+import gitLogo from '../assets/logos/gitLogo.png'
+import gitHubLogo from '../assets/logos/gitHubLogo.png'
+import htmlLogo from '../assets/logos/htmlLogo.png'
 
 export default function Introduction() {
   const [showButton, setShowButton] = useState(false)
@@ -34,6 +41,44 @@ export default function Introduction() {
         <img src={mjHeadShot} alt="a picture of MJ" className="intro__img" />
       </section>
 
+      <section className="tech-stack">
+        <h3 className="logos-heading">🛠️ Tech Stack</h3>
+        <div className="logos-container">
+          <a href="https://react.dev/">
+            <img src={reactLogo} title="React" alt="React Logo" />
+          </a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+            <img src={cssLogo} title="CSS3" alt="CSS Logo" />
+          </a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
+            <img src={htmlLogo} title="HTML5" alt="HTML Logo" />
+          </a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/javascript">
+            <img
+              src={javaScriptLogo}
+              title="JavaScript"
+              alt="JavaScript Logo"
+            />
+          </a>
+          <a href="https://firebase.google.com/">
+            <img src={firebaseLogo} title="Firebase" alt="Firebase Logo" />
+          </a>
+          <a href="https://git-scm.com/">
+            <img src={gitLogo} title="Git" alt="Git Logo" />
+          </a>
+          <a href="https://github.com/mjlearn13">
+            <img src={gitHubLogo} title="Git" alt="Git Logo" />
+          </a>
+          <a href="https://reactrouter.com/en/main">
+            <img
+              src="https://cdn.freebiesupply.com/logos/thumbs/2x/react-router-logo.png"
+              title="React Router"
+              alt="React Router Logo"
+            />
+          </a>
+        </div>
+      </section>
+
       <section className="about-me">
         <h2 className="section__title section__title--about">Who I am</h2>
         <p className="section__subtitle section__subtitle--about">
@@ -42,18 +87,18 @@ export default function Introduction() {
 
         <div>
           <p>
-            I&apos;m a Front-End React Developer based out of Berkshire, UK. My specialities
-            include React JS, JavaScript, CSS and HTML. My focus is on
-            developing user-friendly web applications with a strong emphasis on
-            responsiveness.
+            I&apos;m a Front-End React Developer based out of Berkshire, UK. My
+            specialities include React JS, JavaScript, CSS and HTML. My focus is
+            on developing user-friendly web applications with a strong emphasis
+            on responsiveness.
           </p>
           <p>
-            I&apos;m passionate, self-motivated and eager to
-            learn and develop.
+            I&apos;m passionate, self-motivated and eager to learn and develop.
           </p>
           <p>
-            I was a London Fire-Fighter for 14 years and also a plumber, carrying out heating and gas work. 
-            I have an Associate in Specialised Technology degree in Personal Computer and Network
+            I was a London Fire-Fighter for 14 years and also a plumber,
+            carrying out heating and gas work. I have an Associate in
+            Specialised Technology degree in Personal Computer and Network
             Technology from Allentown Business School. I&apos;m a purple belt in
             Brazilian Jiu-Jitsu and a wrestler.
           </p>
